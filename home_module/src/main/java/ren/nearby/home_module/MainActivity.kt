ae6352084ab710695994dd247ea4a50ccfbfcbf1
lr <wangcunrong@ejiayou.com>
2022-02-24 15:50:35 +0800

    ...

---
+++ home_module/src/main/java/ren/nearby/home_module/MainActivity.kt
@@ -0,0 +1,19 @@
+package ren.nearby.home_module
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ren.nearby.common_module.BaseActivityKot
+
+
+/**
+ * @author:
+ * @created on: 2022/2/24 11:28
+ * @description:
+ */
+class MainActivity : BaseActivityKot() {
+
+    override fun initView() {
+        super.initView()
+    }
+
+}
