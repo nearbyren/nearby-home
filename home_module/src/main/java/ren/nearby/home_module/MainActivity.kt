765bd43918f50c00e9b3fb3f39776e20c1da6dd7
lr <nearby.ren@gmail.com>
2022-02-25 22:18:56 +0800

    ...

--- home_module/src/main/java/ren/nearby/home_module/MainActivity.kt
+++ home_module/src/main/java/ren/nearby/home_module/MainActivity.kt
@@ -1,12 +1,9 @@
 package ren.nearby.home_module
 
-import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.orhanobut.logger.Logger
 import ren.nearby.common_module.BaseActivityKot
 import ren.nearby.share_export.ShareServiceUtil
-import ren.nearby.share_export.router.ShareRouterTable
 
 
 /**
@@ -19,10 +16,15 @@ class MainActivity : BaseActivityKot() {
 
     override fun initView() {
         super.initView()
-        Toast.makeText(MainActivity@this,"1",Toast.LENGTH_LONG).show()
+
         Logger.d("share share_export initView")
         ShareServiceUtil.navigateShareWx()
-        ShareServiceUtil.shareWxResult()
+
+        Toast.makeText(
+            MainActivity@ this,
+            "分享 结果    =  ${ShareServiceUtil.shareWxResult()}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
 }
